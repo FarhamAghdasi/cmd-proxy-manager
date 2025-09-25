4097e8e881208b65a5cca2b9da03cc11326902e5
Farham Aghdasi <farhamaghdasi08@gmail.com>
2025-09-25 18:01:19 +0330

    feat: added run file

---
+++ proxy_manager.bat
@@ -0,0 +1,9 @@
+@echo off
+:: set title app
+title Python CLI Proxy manager
+
+:: Run the main.py script with optional GUI
+python "proxy_manager.py"
+
+:: Pause the script to keep the window open after execution
+pause
